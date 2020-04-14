f8a8c1fade9e89374592ecb4aad0f801c1c0eab6
Hohungduy <50991312+Hohungduy@users.noreply.github.com>
2020-04-14 09:54:51 +0700

    Delete .mycrypto.ko.cmd

--- .mycrypto.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/duyho/My-linux-crypto-driver_prototype/mycrypto.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/duyho/My-linux-crypto-driver_prototype/mycrypto.ko /home/duyho/My-linux-crypto-driver_prototype/mycrypto.o /home/duyho/My-linux-crypto-driver_prototype/mycrypto.mod.o ;  true
